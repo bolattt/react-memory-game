d8a9672c1e9251549787e9b4a9e8d93f0dda034b
Bo Latt <bolattny@gmail.com>
2022-10-15 04:33:31 -0400

    initial commit

---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,13 @@
+import './SingleCard.css'
+const SingleCard = ({card, handleChoice }) =>  { 
+  const handleClick = () => { 
+    handleChoice(card)
+  }
+    return  ( 
+        <div className='card' >
+          <img className='front' src={card.src} alt="card front" />
+          <img onClick={handleClick} className='back' src='/img/cover.png' alt="card back" />
+        </div>
+      )
+}
+export default SingleCard;
